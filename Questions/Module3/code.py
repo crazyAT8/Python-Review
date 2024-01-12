8bd0f7c5fdfcc7c1507a1284e097190bd2fbebee
crazyAT8 <88wwatson@gmail.com>
2024-01-11 23:59:14 -0600

    1/11/24

--- Questions/Module3/code.py
+++ Questions/Module3/code.py
@@ -35,11 +35,55 @@ What is the output?
 '''
 
 
-my_list = [1, 2, 3]
-print(len(my_list))
-for v in range(len(my_list)):
-    # print(v)
-    print(my_list[v])
-    my_list.insert(1, my_list[v])
-print(my_list)
+# my_list = [1, 2, 3]
+# print(len(my_list))
+# for v in range(len(my_list)):
+#     # print(v)
+#     print(my_list[v])
+#     my_list.insert(1, my_list[v])
+# print(my_list)
 
+
+# 17
+
+''' 
+After execution of the following snippet,
+the sum of all "vals" elements 
+will be equal to:
+'''
+# vals = [0, 1, 2]
+# vals.insert(0, 1)
+# del vals[1]
+# print(vals)
+
+'''  
+ What would the code be to loop through
+ the list to add up all of the values
+ '''
+
+# total = 0
+# for val in vals:
+#     total += val 
+
+# print("Sum of all elements:", total)
+
+
+#18 
+'''  
+What is the output:
+    o [1, 1, 1]
+    o [1, 2, 3]
+    o [3, 3, 3]
+    o [3, 2, 1]
+'''
+
+# my_list_1 = [1, 2, 3]
+# my_list_2 = []
+# for v in my_list_1:
+#     my_list_2.insert(0, v) 
+# print(my_list_2)
+
+vals2 = [0,1,2]
+vals2[0], vals2[2] = vals2[2], vals2[0]
+
+print(vals2)
